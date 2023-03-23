46cde7ade083ce09fab2e471c7102812488346c0
Lukas Mäglin <lm@cyon.ch>
2023-03-23 16:50:43 +0100

    Min change: dotted separation line instead of solid.

--- src/PaymentPart/Output/XslFoOutput/Template/PrintableBordersTemplate.php
+++ src/PaymentPart/Output/XslFoOutput/Template/PrintableBordersTemplate.php
@@ -5,6 +5,6 @@ namespace Sprain\SwissQrBill\PaymentPart\Output\XslFoOutput\Template;
 class PrintableBordersTemplate
 {
     public const TEMPLATE = <<<EOT
-0.2mm solid black
+0.2mm dotted black
 EOT;
 }
